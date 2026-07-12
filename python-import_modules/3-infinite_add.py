f3309aa6b6cc9c8b53837ec41790e34e7a41a8b8
luckysitara <udoessienprecious@gmail.com>
2026-07-12 02:27:55 -0700

    3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""Print the number of and list of arguments."""
+"""Print the result of the addition of all arguments."""
 
 
 import sys
 
 
 if __name__ == "__main__":
-    count = len(sys.argv) - 1
+    result = 0
 
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
+    for arg in sys.argv[1:]:
+        result += int(arg)
 
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    print(result)
