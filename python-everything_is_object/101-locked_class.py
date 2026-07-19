bb8e84c2fd6f852c1429a4af36a2dc96a0c2e904
luckysitara <udoessienprecious@gmail.com>
2026-07-19 01:04:30 -0700

    101-locked_class.py

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""Defines a class with restricted instance attributes"""
+
+
 class LockedClass:
+    """Prevents dynamic creation of instance attributes,
+    except for 'first_name'
+    """
     __slots__ = ["first_name"]
