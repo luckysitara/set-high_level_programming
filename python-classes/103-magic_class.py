60f8e30ba690801a1c1ef6b61e8b85f9cf790e67
luckysitara <udoessienprecious@gmail.com>
2026-07-19 00:27:06 -0700

    101-square.py

--- python-classes/103-magic_class.py
+++ python-classes/103-magic_class.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""A class representing a magical circle with radius."""
+"""Defines MagicClass"""
+import math
+
+
 class MagicClass:
+    """Defines a magic class representing a circle"""
 
-
-    def __init__(self, radius):
-        """Initialize a MagicClass instance with a given radius.:param radius: The radius of the circle."""
+    def __init__(self, radius=0):
+        """Initialize a new MagicClass"""
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        """Calculate the area of the circle.Formula: A = π * r^2"""
-        return self.__radius * self.__radius * 3.14
+        """Return the area of the circle"""
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        """Calculate the circumference of the circle Formula: C = 2 * π * """
-        return 2 * 3.14 * self.__radius
+        """Return the circumference of the circle"""
+        return 2 * math.pi * self.__radius
