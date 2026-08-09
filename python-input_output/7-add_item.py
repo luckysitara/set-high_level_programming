501b3ad295731ac31ba5231340a34a918f101b65
luckysitara <udoessienprecious@gmail.com>
2026-08-08 17:16:00 -0700

    0-read_file.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,5 @@
-
 #!/usr/bin/python3
-"""
-Write a script that adds all
-arguments to a Python list, and then
-save them to a file
-"""
+"""Script that adds all arguments to a Python list, and saves them to a file."""
 from sys import argv
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
