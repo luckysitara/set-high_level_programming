282fbfdcc256122425dfa5d5d093e4e24ea26bcf
luckysitara <udoessienprecious@gmail.com>
2026-08-08 17:13:26 -0700

    0-read_file.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,25 +1,25 @@
+
 #!/usr/bin/python3
-
-from sys import argv
-import json
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
-'''
+"""
 Write a script that adds all
 arguments to a Python list, and then
 save them to a file
-'''
+"""
+from sys import argv
 
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-argc = len(argv)
 
 filename = 'add_item.json'
 my_list = []
+
 try:
     my_list = load_from_json_file(filename)
-except BaseException:
+except Exception:
     pass
-for items in range(1, argc):
+
+for items in range(1, len(argv)):
     my_list.append(argv[items])
+
 save_to_json_file(my_list, filename)
