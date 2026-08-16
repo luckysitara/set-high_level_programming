08f3ed436c115678a5a8711090de79ca539883ee
luckysitara <udoessienprecious@gmail.com>
2026-08-15 19:44:53 -0700

    fixed

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -7,8 +7,14 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
-                            user=argv[1], passwd=argv[2], db=argv[3])
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        charset="utf8",
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
     cur = conn.cursor()
     query = (
         "SELECT cities.id, cities.name, states.name "
