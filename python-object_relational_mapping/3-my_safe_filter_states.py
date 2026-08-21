57bbcb6f8c900670fd3ff293fd29e21c3c507a38
luckysitara <udoessienprecious@gmail.com>
2026-08-21 02:34:26 -0700

    3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,7 +5,6 @@ Take in an argument and make it safe from MySQL injections.
 import MySQLdb
 from sys import argv
 
-
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                             user=argv[1], passwd=argv[2], db=argv[3])
