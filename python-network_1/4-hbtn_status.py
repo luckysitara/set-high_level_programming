1c8a6cbea88b6db1ad056a387060d7c590950131
luckysitara <udoessienprecious@gmail.com>
2026-08-21 04:23:17 -0700

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Fetch https://intranet.hbtn.io/status using requests."""
+"""Fetch https://alx-intranet.hbtn.io/status using requests."""
 import requests
 
 
 if __name__ == "__main__":
-    response = requests.get("https://intranet.hbtn.io/status")
+    response = requests.get("https://alx-intranet.hbtn.io/status")
     body = response.text
     print("Body response:")
     print("\t- type: {}".format(type(body)))
