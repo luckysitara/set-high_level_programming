7accf149dc1e5d285ed2b1085c4b49230fb7aa28
luckysitara <udoessienprecious@gmail.com>
2026-08-21 03:14:44 -0700

    0-hbtn_status.ph

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,9 +2,16 @@
 """Fetch https://alx-intranet.hbtn.io/status and display the response body."""
 import urllib.request
 
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(body)))
-    print("\t- content: {}".format(body))
-    print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+def fetch_status():
+    """Fetch the status endpoint and print the response body details."""
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+
+
+if __name__ == "__main__":
+    fetch_status()
