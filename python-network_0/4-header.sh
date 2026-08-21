798b1f4fb30372bf92890ab964065fc153dd1318
luckysitara <udoessienprecious@gmail.com>
2026-08-21 02:56:44 -0700

    4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-#Takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response.
-curl -s "$1" -X GET -H "X-HelloSchool!-User-Id: 98"
+# Takes in a URL as an argument, sends a GET request to the URL,
+# and displays the body of the response, with a custom header.
+curl -s -X GET -H "X-School-User-Id: 98" "$1"
